2436bc4be6f28d199fcc58ef39060d860fe94baf
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-03-02 13:06:32 -0900

    Add box shadows and a home logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import { PostList } from './features/posts/PostList';
+import PostList from './features/posts/PostList';
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { selectPosts, fetchPosts } from './features/posts/postsSlice';
 import { useEffect } from 'react';
 import { AppDispatch } from './app/store';
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet, NavLink } from 'react-router-dom';
 import PostPage from './features/posts/PostPage';
+import Header from './features/search/Header';
+import FilterBar from './Components/FilterBar';
+import { BsHouseDoor } from 'react-icons/bs';
 
 
 export default function App() {
@@ -31,7 +34,27 @@ export default function App() {
 function Layout() {
   return (
     <div className="App">
-      <Outlet />
+      <Header />
+      <aside>
+        <div className="subredditLink">
+          <span className="feeds">FEEDS</span>
+          <NavLink to="/" >
+            <div className="home">
+              <BsHouseDoor />
+              Home
+            </div>
+          </NavLink>
+          <div className='line'></div>
+          <NavLink to="/" >r/C_Programming</NavLink>
+          <NavLink to="/" >r/computerarchitecture</NavLink>
+          <NavLink to="/" >r/ECE</NavLink>
+          <NavLink to="/" >r/embedded</NavLink>
+        </div>
+      </aside>
+      <div className="postSection">
+        <FilterBar />
+        <Outlet />
+      </div>
     </div>
   )
 }
