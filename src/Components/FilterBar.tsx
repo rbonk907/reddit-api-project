4322598b1f533467e28c4b3bdda90e020eb51bc0
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-03-06 14:50:56 -0900

    Style Navlinks on Filter bar

--- src/Components/FilterBar.tsx
+++ src/Components/FilterBar.tsx
@@ -1,21 +1,36 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import styles from "./FilterBar.module.css";
 import { MdLocalFireDepartment, MdLeaderboard, MdOutlineAutoAwesome } from "react-icons/md";
 
+interface FilterBarProps {
+    handleClick: (subreddit: string, filter: string) => void;
+}
 
-export default function FilterBar() {
+export default function FilterBar({ handleClick }: FilterBarProps) {
+    const { subreddit } = useParams();
 
     return (
         <div className={`${styles.filterBar} ${styles.boxShadow}`}>
-            <NavLink to="/">
-                <div className={styles.icon}><MdLocalFireDepartment /></div>
+            <NavLink 
+                to={ subreddit ? `${subreddit}/hot/` : '/'}
+                onClick={subreddit ? () => handleClick(subreddit, 'hot') : undefined }
+                className={({ isActive }) => isActive ? styles.linkActive : undefined} >
+                <div className={styles.icon}>
+                    <MdLocalFireDepartment />
+                </div>
                 <span>Hot</span>
             </NavLink>
-            <NavLink to="/">
+            <NavLink 
+                to={ subreddit ? `${subreddit}/new/` : 'new/'}
+                onClick={subreddit ? () => handleClick(subreddit, 'new') : undefined }
+                className={({ isActive }) => isActive ? styles.linkActive : undefined} >
                 <div className={styles.icon}><MdOutlineAutoAwesome /></div>
                 <span>New</span>
             </NavLink>
-            <NavLink to="/">
+            <NavLink 
+                to={ subreddit ? `${subreddit}/top/` : 'top/'}
+                onClick={subreddit ? () => handleClick(subreddit, 'top') : undefined }
+                className={({ isActive }) => isActive ? styles.linkActive : undefined} >
                 <div className={styles.icon}><MdLeaderboard /></div>
                 <span>Top</span>
             </NavLink>
